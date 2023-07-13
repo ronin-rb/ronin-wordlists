cd565ba00b7e6c318d0bb1921e815ad6b4aeda81
Postmodern <postmodern.mod3@gmail.com>
2023-07-12 19:29:08 -0700

    Added validation specs for `data/wordlists.yml`.

---
+++ spec/wordlists_yml_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+require 'yaml'
+
+describe "data/wordlists.yml" do
+  wordlists = YAML.load_file(File.join(__dir__,'..','data','wordlists.yml'))
+
+  subject { wordlists }
+
+  it "must contain a Hash" do
+    expect(subject).to be_kind_of(Hash)
+    expect(subject).to_not be_empty
+  end
+
+  wordlists.each do |name,attributes|
+    describe(name) do
+      let(:name) { name }
+      let(:attributes) { attributes }
+
+      it "must have a valid name" do
+        expect(name).to match(/\A[A-Za-z0-9_-]+\z/)
+      end
+
+      it "must contain a Hash of attributes" do
+        expect(attributes).to be_kind_of(Hash)
+      end
+
+      it "must use Symbols for it's attribute names" do
+        expect(attributes.keys).to all(be_kind_of(Symbol))
+      end
+
+      it "must have a valid :url" do
+        expect(attributes[:url]).to match(/\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/)
+      end
+
+      it "must have :categories" do
+        expect(attributes[:categories]).to be_kind_of(Array)
+        expect(attributes[:categories]).to_not be_empty
+        expect(attributes[:categories]).to all(be_kind_of(String))
+      end
+
+      it "must have a :summary" do
+        expect(attributes[:summary]).to be_kind_of(String)
+        expect(attributes[:summary]).to_not be_empty
+        expect(attributes[:summary]).to end_with('.')
+      end
+    end
+  end
+end
