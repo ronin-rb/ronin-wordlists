9e788c3031e5acdc58edad711bf048f808fe8b51
AI-Mozi <bieniek.milosz0@gmail.com>
2024-01-25 20:35:58 +0100

    Add spec for `List`

--- spec/cli/commands/list_spec.rb
+++ spec/cli/commands/list_spec.rb
@@ -4,4 +4,29 @@ require_relative 'man_page_example'
 
 describe Ronin::Wordlists::CLI::Commands::List do
   include_examples "man_page"
+
+  describe '#run' do
+    context 'with given name' do
+      let(:name)      { 'list' }
+      let(:wordlists) { ['list1', 'list2'] }
+
+      it 'must list wordlists matching the specified name' do
+        expect(subject.wordlist_dir).to receive(:list).with(name).and_return(wordlists)
+        expect {
+          subject.run(name)
+        }.to output("  list1\n  list2\n").to_stdout
+      end
+    end
+
+    context 'witout given name' do
+      let(:wordlists) { ['wordlist1', 'wordlist2', 'wordlist3'] }
+
+      it 'must list all wordlists' do
+        expect(subject.wordlist_dir).to receive(:list).with(no_args).and_return(wordlists)
+        expect {
+          subject.run
+        }.to output("  wordlist1\n  wordlist2\n  wordlist3\n").to_stdout
+      end
+    end
+  end
 end
