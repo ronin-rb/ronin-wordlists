c99cdb38735734bd9bdca0c6f4a0576155a6cc02
AI-Mozi <bieniek.milosz0@gmail.com>
2024-01-25 16:58:28 +0100

    Change class name and its usage

--- spec/cli/wordlist_option_spec.rb
+++ spec/cli/wordlist_option_spec.rb
@@ -3,12 +3,14 @@ require 'ronin/wordlists/cli/wordlist_option'
 require 'ronin/wordlists/cli/command'
 
 RSpec.describe Ronin::Wordlists::CLI::WordlistOption do
-  class WordlistOptionCommandMock < Ronin::Wordlists::CLI::Command
+  class TestWordlistOptionCommand < Ronin::Wordlists::CLI::Command
     include Ronin::Wordlists::CLI::WordlistOption
   end
 
+  let(:command_class) { TestWordlistOptionCommand }
+
   describe '.included' do
-    subject { WordlistOptionCommandMock }
+    subject { command_class }
 
     it 'adds the --wordlist option to the command' do
       expect(subject.options).to include(:wordlist)
