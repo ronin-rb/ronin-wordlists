93aa38c05a1784f8bae444bd234230c2e22ccf3e
Miłosz Bieniek <bieniek.milosz0@gmail.com>
2024-01-26 10:28:05 +0100

    Add spec for `Purge` (#25)

--- spec/cli/commands/purge_spec.rb
+++ spec/cli/commands/purge_spec.rb
@@ -4,4 +4,18 @@ require_relative 'man_page_example'
 
 describe Ronin::Wordlists::CLI::Commands::Purge do
   include_examples "man_page"
+
+  describe '#run' do
+    let(:cache_dir) { instance_double('CacheDir') }
+
+    before do
+      allow(Ronin::Wordlists::CacheDir).to receive(:new).and_return(cache_dir)
+    end
+
+    it 'must purge all cached wordlists' do
+      expect(cache_dir).to receive(:purge)
+
+      subject.run
+    end
+  end
 end
