a84c1eb2f562ee23374023986518d592560d24e5
AI-Mozi <bieniek.milosz0@gmail.com>
2024-01-26 10:33:20 +0100

    Add spec for `Remove`

--- spec/cli/commands/remove_spec.rb
+++ spec/cli/commands/remove_spec.rb
@@ -4,4 +4,31 @@ require_relative 'man_page_example'
 
 describe Ronin::Wordlists::CLI::Commands::Remove do
   include_examples "man_page"
+
+  let(:wordlist_dir) { double('WordlistDir') }
+
+  describe '#run' do
+    context 'when the wordlist exists' do
+      let(:wordlist_name) { 'wordlist' }
+
+      before do
+        allow(subject).to receive(:wordlist_dir).and_return(wordlist_dir)
+      end
+
+      it 'must remove it from cache directory' do
+        expect(wordlist_dir).to receive(:remove).with(wordlist_name)
+        subject.run(wordlist_name)
+      end
+    end
+
+    context 'when the wordlist does not exist' do
+      let(:wordlist_name) { 'not_existing_wordlist'}
+
+      it 'must print an error message and exit' do
+        expect {
+          subject.run(wordlist_name)
+        }.to output("no such wordlist #{wordlist_name}").to_stderr
+      end
+    end
+  end
 end
